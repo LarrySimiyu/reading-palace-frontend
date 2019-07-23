c37139c297d27cb9520fc1ffdd09c04e93fc1ac5
LarrySimiyu <larry.simiyu@gmail.com>
2019-07-22 19:01:59 -0700

    on change added

--- reading-palace/src/components/Screenplay.js
+++ reading-palace/src/components/Screenplay.js
@@ -4,7 +4,11 @@ import axios from "axios";
 class Screenplay extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      title: "",
+      description: "",
+      category: ""
+    };
   }
 
   handleInputChange = event => {
@@ -13,11 +17,37 @@ class Screenplay extends Component {
     });
   };
 
+  submitFriend = event => {};
+
   render() {
     return (
       <div>
         <h1>Submit Screenplay</h1>
-        <input />
+        <input
+          type="text"
+          name="title"
+          value={this.state.title}
+          onChange={this.handleInputChange}
+          id="title"
+          placeholder="title"
+        />
+        <input
+          type="text"
+          name="description"
+          value={this.state.description}
+          onChange={this.handleInputChange}
+          id="description"
+          placeholder="title"
+        />
+        <input
+          type="text"
+          name="category"
+          value={this.state.category}
+          onChange={this.handleInputChange}
+          id="category"
+          placeholder="category"
+        />
+        <button />
       </div>
     );
   }
