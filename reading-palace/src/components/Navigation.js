c10b61e6794bc8649abef58eaa038be58930d4e3
LarrySimiyu <larry.simiyu@gmail.com>
2019-07-23 19:43:26 -0700

    log out functionality added

--- reading-palace/src/components/Navigation.js
+++ reading-palace/src/components/Navigation.js
@@ -1,15 +1,27 @@
-import React, { Component } from 'react';
-import { NavLink, withRouter } from 'react-router-dom';
-import axios from 'axios';
+import React, { Component } from "react";
+import { NavLink, withRouter } from "react-router-dom";
+import axios from "axios";
 
 class Navigation extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-    }
-    render() { 
-        return (  );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  logOut = () => {
+    window.localStorage.clear();
+    this.props.history.push("/");
+  };
+
+  render() {
+    return (
+      <nav>
+        <div>
+          <NavLink to="#">Hitlist</NavLink>
+        </div>
+      </nav>
+    );
+  }
 }
- 
+
 export default Navigation;
