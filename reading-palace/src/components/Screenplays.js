b68c25395354d28d170991a7eba85f412491854a
LarrySimiyu <larry.simiyu@gmail.com>
2019-10-06 18:51:13 -0700

    axios requrest functionig, refactor

--- reading-palace/src/components/Screenplays.js
+++ reading-palace/src/components/Screenplays.js
@@ -5,7 +5,8 @@ class Screenplays extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      screenplays: ["larry", "movie two", "movie three", "movie four"]
+      //screenplays: ["larry", "movie two", "movie three", "movie four"]
+      users: []
     };
   }
 
@@ -13,7 +14,15 @@ class Screenplays extends Component {
     this.getScreenplays();
   }
 
-  getScreenplays = () => {};
+  getScreenplays = () => {
+    axios
+      .get("https://reqres.in/api/users?page=1")
+      .then(data => this.setState({ users: data.data.data }))
+      .catch(error => {
+        console.log(error);
+        return null;
+      });
+  };
 
   handleInputChange = event => {
     this.setState({
@@ -24,10 +33,13 @@ class Screenplays extends Component {
   render() {
     return (
       <div>
-        <h1>Screenplays</h1>
-        {this.state.screenplays.map(screenplay => {
-          return <div>{screenplay}</div>;
-        })}
+        {this.state.users.length === 0 ? (
+          <div>Loading...</div>
+        ) : (
+          this.state.users.map((e, i) => {
+            return <div key={i}>{e.last_name}</div>;
+          })
+        )}
       </div>
     );
   }
