860f1798b3f1d3d8e273b0952627c57a3413f95f
LarrySimiyu <larry.simiyu@gmail.com>
2019-10-11 16:56:21 -0700

    moving add screenplay component

---
+++ reading-palace/src/components/AddScreenplay.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class AddScreenplay extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            title:"",
+            screenwriter:"",
+            co_author:"",
+            description:"",
+            category:""
+         }
+    }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default AddScreenplay;
+title:"",
+screenwriter:"",
+co_author:"",
+description:"",
+category:""
