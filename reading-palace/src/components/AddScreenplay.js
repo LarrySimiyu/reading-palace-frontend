c7ecd10dd23db1cf9118a890c656b9a85f9b4d74
LarrySimiyu <larry.simiyu@gmail.com>
2019-10-11 17:02:16 -0700

    new input fields created

--- reading-palace/src/components/AddScreenplay.js
+++ reading-palace/src/components/AddScreenplay.js
@@ -1,20 +1,67 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 class AddScreenplay extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            title:"",
-            screenwriter:"",
-            co_author:"",
-            description:"",
-            category:""
-         }
-    }
-    render() { 
-        return (  );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      screenwriter: "",
+      co_author: "",
+      description: "",
+      category: ""
+    };
+  }
+  render() {
+    return (
+      <div>
+        <form>
+          <input
+            type="text"
+            name="title"
+            value={this.state.title}
+            onChange={this.handleInputChange}
+            id="title"
+            placeholder="Title"
+          />
+
+          <input
+            type="text"
+            name="screenwriter"
+            value={this.state.screenwriter}
+            onChange={this.handleInputChange}
+            id="screenwriter"
+            placeholder="Screenwriter"
+          />
+          <input
+            type="text"
+            name="co_writer"
+            value={this.state.co_writer}
+            onChange={this.handleInputChange}
+            id="co_writer"
+            placeholder="Co Writer"
+          />
+          <input
+            type="text"
+            name="description"
+            value={this.state.description}
+            onChange={this.handleInputChange}
+            id="description"
+            placeholder="Description"
+          />
+          <input
+            type="text"
+            name="category"
+            value={this.state.category}
+            onChange={this.handleInputChange}
+            id="category"
+            placeholder="category"
+          />
+          <button onClick={this.addScreenplay}>Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
- 
+
 export default AddScreenplay;
