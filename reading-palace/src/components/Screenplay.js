be7e33c3b00921b487589b51b61a03b02ede6cab
LarrySimiyu <larry.simiyu@gmail.com>
2019-10-13 12:56:46 -0700

    screenplay card logic added

--- reading-palace/src/components/Screenplay.js
+++ reading-palace/src/components/Screenplay.js
@@ -23,7 +23,23 @@ class Screenplay extends Component {
   }
 
   render() {
-    return <div>Screenplay</div>;
+    return (
+      <div>
+        <div>
+          <h1>Screenplay Info</h1>
+        </div>
+        <div>{this.state.screenplay.title}</div>
+        <div>
+          {this.state.screenplay.co_author.length > 0 ? (
+            <div>{this.state.screenplay.co_author}</div>
+          ) : (
+            <div>No Co Author</div>
+          )}
+        </div>
+        <div>{this.state.screenplay.description}</div>
+        <div>{this.state.screenplay.category}</div>
+      </div>
+    );
   }
 }
 
