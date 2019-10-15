1a38f8463372fa2c11c5f9f4034217bc74fbfd3f
LarrySimiyu <larry.simiyu@gmail.com>
2019-10-15 15:43:45 -0700

    file import order changed

--- reading-palace/src/App.js
+++ reading-palace/src/App.js
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import "./App.css";
 
-import Screenplays from "./components/Screenplays";
 import Navigation from "./components/Navigation";
+
 import SearchScreenplays from "./components/searchScreenplay";
 import Screenplay from "./components/Screenplay";
+import AddScreenplay from "./components/AddScreenplay";
+import Screenplays from "./components/Screenplays";
+
 import Hitlist from "./components/Hitlist";
+import AddHitlist from "./components/AddHitList";
 
 function App() {
   // const [state, setState] = useState([{id: 1, name: 'Hello'}])
