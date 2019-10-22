30734aca14b35712d2d431cb53f82822c60b4f9d
LarrySimiyu <larry.simiyu@gmail.com>
2019-10-22 11:44:09 -0700

    hitlist error fixed

--- reading-palace/src/components/AddScreenplay.js
+++ reading-palace/src/components/AddScreenplay.js
@@ -32,7 +32,7 @@ class AddScreenplay extends Component {
     };
 
     axios
-      .post("https://script-palace.herokuapp.com/api/hitList", newScreenplay)
+      .post("https://script-palace.herokuapp.com/api/filmInfo", newScreenplay)
       .then(response => {
         if (response.status === 200) {
           alert("screenplay was added");
