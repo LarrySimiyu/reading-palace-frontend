7ca88863f41364e5273a24c0e52889d7fe4dfb05
LarrySimiyu <larry.simiyu@gmail.com>
2019-10-25 23:13:56 -0700

    screenplay errir saved

--- reading-palace/src/components/Screenplay.js
+++ reading-palace/src/components/Screenplay.js
@@ -9,18 +9,18 @@ class Screenplay extends Component {
     };
   }
 
-  componentDidMount() {
-    const id = this.props.location.pathname.split("/")[2];
+  // componentDidMount() {
+  //   const id = this.props.location.pathname.split("/")[2];
 
-    axios
-      .get(`https://movie-palace.herokuapp.com/api/filmInfo/${id}`)
-      .then(response => {
-        this.setState({
-          screenplay: response.data[0]
-        });
-      })
-      .catch(error => console.log(error));
-  }
+  //   axios
+  //     .get(`https://movie-palace.herokuapp.com/api/filmInfo/${id}`)
+  //     .then(response => {
+  //       this.setState({
+  //         screenplay: response.data[0]
+  //       });
+  //     })
+  //     .catch(error => console.log(error));
+  // }
 
   render() {
     return (
@@ -28,10 +28,10 @@ class Screenplay extends Component {
         <div>
           <h1>Screenplay Info</h1>
         </div>
-        <div>{this.state.screenplay.title}</div>
-        <div>{this.state.screenplay.co_author}</div>
-        <div>{this.state.screenplay.description}</div>
-        <div>{this.state.screenplay.category}</div>
+        <div>Title: Larry Simiyu{this.state.screenplay.title}</div>
+        <div>Co-Author: Nobody Bidness{this.state.screenplay.coauthor}</div>
+        <div>Description: Here for now{this.state.screenplay.description}</div>
+        <div>Category: Mategory {this.state.screenplay.category}</div>
       </div>
     );
   }
