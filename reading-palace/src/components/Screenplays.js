c1e3cfb70c87c8ca8b7a7fd053feafd86b4b1508
LarrySimiyu <larry.simiyu@gmail.com>
2019-11-01 15:03:01 -0700

    pdfurl fixed

--- reading-palace/src/components/Screenplays.js
+++ reading-palace/src/components/Screenplays.js
@@ -43,7 +43,7 @@ class Screenplays extends Component {
                 <div>Cowriter: {screenplay.cowriter}</div>
                 <div>Description: {screenplay.description}</div>
                 <div>Category: {screenplay.category}</div>
-                <div>{screenplay.pdf}</div>
+                <div>{screenplay.pdfurl}</div>
                 <div>
                   <Link to={`/screenplay/${screenplay.id}`}>
                     <button>View</button>
