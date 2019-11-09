349881b54312128931d5bdb145f94dcfc584e419
LarrySimiyu <larry.simiyu@gmail.com>
2019-11-09 14:52:59 -0800

    containers created

--- reading-palace/src/components/AddScreenplay.js
+++ reading-palace/src/components/AddScreenplay.js
@@ -72,86 +72,88 @@ class AddScreenplay extends Component {
   render() {
     return (
       <div>
-        <div className="screenplay-form">
-          <Form onSubmit={this.handleSubmit}>
-            <FormGroup>
-              <Label for="title">Title</Label>
+        <div className="page-container">
+          <div className="screenplay-form">
+            <Form onSubmit={this.handleSubmit}>
+              <FormGroup>
+                <Label for="title">Title</Label>
 
-              <Input
-                type="text"
-                name="title"
-                value={this.state.title}
-                onChange={this.handleInputChange}
-                id="title"
-                placeholder="Title"
-              />
-            </FormGroup>
+                <Input
+                  type="text"
+                  name="title"
+                  value={this.state.title}
+                  onChange={this.handleInputChange}
+                  id="title"
+                  placeholder="Title"
+                />
+              </FormGroup>
 
-            <FormGroup>
-              <Label for="screenwriter">Screenwriter</Label>
+              <FormGroup>
+                <Label for="screenwriter">Screenwriter</Label>
 
-              <Input
-                type="text"
-                name="screenwriter"
-                value={this.state.screenwriter}
-                onChange={this.handleInputChange}
-                id="screenwriter"
-                placeholder="Screenwriter"
-              />
-            </FormGroup>
+                <Input
+                  type="text"
+                  name="screenwriter"
+                  value={this.state.screenwriter}
+                  onChange={this.handleInputChange}
+                  id="screenwriter"
+                  placeholder="Screenwriter"
+                />
+              </FormGroup>
 
-            <FormGroup>
-              <Label for="cowriter">Co-Writer</Label>
-              <Input
-                type="text"
-                name="cowriter"
-                value={this.state.cowriter}
-                onChange={this.handleInputChange}
-                id="cowriter"
-                placeholder="Co-Writer"
-              />
-            </FormGroup>
+              <FormGroup>
+                <Label for="cowriter">Co-Writer</Label>
+                <Input
+                  type="text"
+                  name="cowriter"
+                  value={this.state.cowriter}
+                  onChange={this.handleInputChange}
+                  id="cowriter"
+                  placeholder="Co-Writer"
+                />
+              </FormGroup>
 
-            <FormGroup>
-              <Label for="description">Description</Label>
-              <Input
-                type="textarea"
-                name="description"
-                value={this.state.description}
-                onChange={this.handleInputChange}
-                id="description"
-                placeholder="Screenplay Description"
-              />
-            </FormGroup>
+              <FormGroup>
+                <Label for="description">Description</Label>
+                <Input
+                  type="textarea"
+                  name="description"
+                  value={this.state.description}
+                  onChange={this.handleInputChange}
+                  id="description"
+                  placeholder="Screenplay Description"
+                />
+              </FormGroup>
 
-            <FormGroup>
-              <Label for="category">Category</Label>
-              <Input
-                type="text"
-                name="category"
-                value={this.state.category}
-                onChange={this.handleInputChange}
-                id="category"
-                placeholder="category"
-              />
-            </FormGroup>
+              <FormGroup>
+                <Label for="category">Category</Label>
+                <Input
+                  type="text"
+                  name="category"
+                  value={this.state.category}
+                  onChange={this.handleInputChange}
+                  id="category"
+                  placeholder="category"
+                />
+              </FormGroup>
 
-            <FormGroup>
-              <Label for="pdfurl">Link</Label>
-              <Input
-                type="text"
-                name="pdfurl"
-                value={this.state.pdfurl}
-                onChange={this.handleInputChange}
-                id="pdfurl"
-                placeholder="Paste PDF Link"
-              />
-            </FormGroup>
+              <FormGroup>
+                <Label for="pdfurl">PDF</Label>
+                <Input
+                  type="text"
+                  name="pdfurl"
+                  value={this.state.pdfurl}
+                  onChange={this.handleInputChange}
+                  id="pdfurl"
+                  placeholder="Paste PDF Link"
+                />
+              </FormGroup>
 
-            <Button color="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
+              <Button color="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </div>
         </div>
       </div>
     );
--- reading-palace/src/components/searchScreenplay.js
+++ reading-palace/src/components/searchScreenplay.js
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 
 import { Input, FormGroup, Label, Form } from "reactstrap";
 
+import "../searchScreenplay.scss";
+
 class searchScreenplay extends Component {
   constructor(props) {
     super(props);
@@ -52,32 +54,34 @@ class searchScreenplay extends Component {
         : this.state.filteredScreenplays;
     return (
       <div>
-        <Form>
-          <FormGroup>
-            <Label>Search By Title</Label>
-            <Input
-              type="text"
-              placeholder="Search"
-              onChange={this.handleInputChange}
-              value={this.state.searchInput}
-              name="searchInput"
-            />
-          </FormGroup>
-        </Form>
+        <div className="search-container">
+          <Form>
+            <FormGroup>
+              <Label>Search By Title</Label>
+              <Input
+                type="text"
+                placeholder="Search"
+                onChange={this.handleInputChange}
+                value={this.state.searchInput}
+                name="searchInput"
+              />
+            </FormGroup>
+          </Form>
 
-        {results.map(screenplay => {
-          return (
-            <div key={screenplay.id} className="screenplay">
-              <Link to={`/screenplay/${screenplay.id}`} className="linkColor">
-                <div>Title: {screenplay.title}</div>
-                <div>screenwriter: {screenplay.screenwriter}</div>
-                <div>Co Author: {screenplay.coauthor}</div>
-                <div>Description: {screenplay.description}</div>
-                <div>Category: {screenplay.catetegory}</div>
-              </Link>
-            </div>
-          );
-        })}
+          {results.map(screenplay => {
+            return (
+              <div key={screenplay.id} className="screenplay">
+                <Link to={`/screenplay/${screenplay.id}`} className="linkColor">
+                  <div>Title: {screenplay.title}</div>
+                  <div>screenwriter: {screenplay.screenwriter}</div>
+                  <div>Co Author: {screenplay.coauthor}</div>
+                  <div>Description: {screenplay.description}</div>
+                  <div>Category: {screenplay.catetegory}</div>
+                </Link>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
