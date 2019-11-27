ec742e1637e49ff0fc612598100a919cef8884de
LarrySimiyu <larry.simiyu@gmail.com>
2019-11-26 18:21:12 -0800

    add hitlist removed

--- reading-palace/src/App.jsx
+++ reading-palace/src/App.jsx
@@ -9,7 +9,6 @@ import Screenplay from "./components/Screenplay";
 import AddScreenplay from "./components/AddScreenplay";
 import Screenplays from "./components/Screenplays";
 
-// import AddHitlist from "./components/AddHitList";
 import searchScreenplay from "./components/searchScreenplay";
 
 class App extends Component {
